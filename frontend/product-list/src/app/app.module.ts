4e3f180f248d75f5f06dee4cd15a0d23b3e91046
Romulo Ciro <romulo.ciro@hotmail.com>
2023-09-06 19:43:47 -0300

    feat: add routes and lazy loading

--- frontend/product-list/src/app/app.module.ts
+++ frontend/product-list/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ToastModule } from 'primeng/toast';
 import { HomeComponent } from './modules/home/home.component';
 import { CommonModule } from '@angular/common';
 import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,7 @@ import { CookieService } from 'ngx-cookie-service';
     ButtonModule,
     ToastModule
   ],
-  providers: [CookieService],
+  providers: [CookieService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
